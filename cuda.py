82c5f363cc8220fab6ac07e011766fb10b4817d3
Sghosh1729 <shreyas.ghosh2021@gmail.com>
2025-05-05 19:20:18 +0530

    Update cuda.py

--- cuda.py
+++ cuda.py
@@ -1,4 +1,4 @@
 import torch
-print(torch.cuda.is_available())  # Should print True if GPU is available
-print(torch.cuda.current_device())  # Get the current GPU device
+print(torch.cuda.is_available())  
+print(torch.cuda.current_device())  
 print(torch.cuda.get_device_name(0))
